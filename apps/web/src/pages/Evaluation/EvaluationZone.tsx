1cdb09d845ab884a5e318246eea9c17f9dad76f0
JeongChan <idgachan@gmail.com>
2026-03-25 17:41:03 +0900

    .
    .

--- apps/web/src/pages/Evaluation/EvaluationZone.tsx
+++ apps/web/src/pages/Evaluation/EvaluationZone.tsx
@@ -289,16 +289,83 @@ const EvaluationZone: React.FC = () => {
           )}
 
           {selectedVote && !currentPost?.hasVoted && (
-            <div className={styles.sliderActionWrap} ref={trackRef}>
-              <div className={styles.sliderTrack} />
+            <div
+              ref={trackRef}
+              className={`${styles.dragTrack} ${
+                isLikeSelected ? styles.dragTrackFromLeft : styles.dragTrackFromRight
+              }`}
+            >
+              <div className={styles.trackText}>평가하러가기</div>
+
+              {isLikeSelected && (
+                <div className={styles.trackHintRight}>
+                  <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+                    <path
+                      d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z"
+                      fill="white"
+                    />
+                  </svg>
+                  <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+                    <path
+                      d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+              )}
+
+              {isDislikeSelected && (
+                <div className={styles.trackHintLeft}>
+                  <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+                    <path
+                      d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z"
+                      fill="white"
+                    />
+                  </svg>
+                  <svg width="22" height="22" viewBox="0 0 24 24" fill="none">
+                    <path
+                      d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+              )}
+
               <motion.div
-                className={styles.sliderThumb}
+                className={styles.dragThumb}
                 drag="x"
-                dragConstraints={{ left: selectedVote === 'LIKE' ? 0 : -maxDrag, right: selectedVote === 'LIKE' ? maxDrag : 0 }}
+                dragMomentum={false}
+                dragConstraints={
+                  isLikeSelected
+                    ? { left: 0, right: maxDrag }
+                    : { left: -maxDrag, right: 0 }
+                }
                 dragElastic={0}
-                style={{ x: dragX }}
+                style={{
+                  x: dragX,
+                  left: isLikeSelected ? 4 : 'auto',
+                  right: isDislikeSelected ? 4 : 'auto',
+                  width: `${THUMB_SIZE}px`,
+                  height: `${THUMB_SIZE}px`,
+                }}
                 onDragEnd={handleDragEnd}
-              />
+              >
+                {isLikeSelected ? (
+                  <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <path
+                      d="M17.5245 8.16619L17.9165 5.78503C18.0193 5.15641 17.9511 4.51172 17.7191 3.9185C17.4871 3.32529 17.0998 2.80539 16.5979 2.41328C16.0959 2.02117 15.4977 1.77129 14.866 1.68982C14.2342 1.60834 13.5922 1.69828 13.0072 1.95019C12.1986 2.31304 11.5491 2.95677 11.179 3.76203L9.00317 8.16619H3.5C2.57174 8.16619 1.6815 8.53494 1.02513 9.19132C0.368749 9.8477 0 10.7379 0 11.6662L0 22.1662C0 23.0945 0.368749 23.9847 1.02513 24.6411C1.6815 25.2974 2.57174 25.6662 3.5 25.6662H25.6877L28 12.7897L28.0187 8.16619H17.5245ZM2.33333 22.1662V11.6662C2.33333 11.3568 2.45625 11.06 2.67504 10.8412C2.89383 10.6224 3.19058 10.4995 3.5 10.4995H8.16667V23.3329H3.5C3.19058 23.3329 2.89383 23.2099 2.67504 22.9912C2.45625 22.7724 2.33333 22.4756 2.33333 22.1662ZM25.6667 12.4829L23.7183 23.3329H10.5V10.3934L13.3222 4.67669C13.4177 4.50378 13.553 4.35602 13.7168 4.24556C13.8805 4.13509 14.0682 4.06508 14.2643 4.04128C14.4605 4.01747 14.6594 4.04056 14.8449 4.10863C15.0304 4.17671 15.197 4.28783 15.3312 4.43286C15.4458 4.56615 15.5296 4.72309 15.5766 4.89249C15.6236 5.06189 15.6326 5.23957 15.603 5.41286L14.7642 10.4995H25.6667V12.4829Z"
+                      fill="black"
+                    />
+                  </svg>
+                ) : (
+                  <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <path
+                      d="M28 16.583L25.6667 3.5H3.5C2.57174 3.5 1.6815 3.86875 1.02513 4.52513C0.368749 5.1815 0 6.07174 0 7L0 17.5C0 18.4283 0.368749 19.3185 1.02513 19.9749C1.6815 20.6313 2.57174 21 3.5 21H8.99733L11.2303 25.5255C11.6069 26.2865 12.2441 26.887 13.0261 27.2178C13.808 27.5486 14.6828 27.5878 15.4912 27.3281C16.2995 27.0683 16.9878 26.5271 17.4308 25.8027C17.8737 25.0784 18.042 24.2191 17.9048 23.3812L17.5128 21H28V16.583ZM2.33333 17.5V7C2.33333 6.69058 2.45625 6.39383 2.67504 6.17504C2.89383 5.95625 3.19058 5.83333 3.5 5.83333H8.16667V18.6667H3.5C3.19058 18.6667 2.89383 18.5437 2.67504 18.325C2.45625 18.1062 2.33333 17.8094 2.33333 17.5ZM25.6667 18.6667H14.7642L15.603 23.7592C15.6326 23.9325 15.6236 24.1101 15.5766 24.2795C15.5296 24.4489 15.4458 24.6059 15.3312 24.7392C15.1966 24.8838 15.0297 24.9945 14.8442 25.0622C14.6586 25.1298 14.4596 25.1525 14.2635 25.1284C14.0675 25.1042 13.8799 25.034 13.7163 24.9233C13.5527 24.8127 13.4176 24.6648 13.3222 24.4918L10.5 18.7728V5.83333H23.7183L25.6667 16.6833V18.6667Z"
+                      fill="black"
+                    />
+                  </svg>
+                )}
+              </motion.div>
             </div>
           )}
         </div>
